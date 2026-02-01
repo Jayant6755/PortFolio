5af70eca74aa37753a5e285984fcb332e8c5bb74
Jayant Negi <jayantnegi28@gmail.com>
2026-02-01 18:42:59 +0530

    again

---
+++ cards-project/src/App.tsx
@@ -0,0 +1,27 @@
+import { Toaster } from "sonner";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./Pages/NotFound";
+import Index from "./Pages/Index";
+
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/not-found" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
